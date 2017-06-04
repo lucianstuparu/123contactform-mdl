af7da14dab0f6935679971ef2165737879da4135
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-04 10:54:27 +0300

    Update contact-form.js

--- contact-form.js
+++ contact-form.js
@@ -1,16 +1,16 @@
-$("#id123-control29537184").attr("placeholder", "Nume si Prenume");
-$('#id123-title29537184').hide();
-$("#id123-control29537184").off("focusin");
-$("#id123-control29537184").off("focusout");
-$("#id123-control29537184").on( "click", function() {
-  if ($("#id123-control29537184") === '') {
-      $('#id123-title29537184').show();
+$("#id123-control29537187").attr("placeholder", "Nume si Prenume");
+$('#id123-title29537187').hide();
+$("#id123-control29537187").off("focusin");
+$("#id123-control29537187").off("focusout");
+$("#id123-control29537187").on( "click", function() {
+  if ($("#id123-control29537187") === '') {
+      $('#id123-title29537187').show();
   }
 });
-$("#id123-control29537184").focusout(function() {
-  if ($("#id123-control29537184") === '') {
-      $('#id123-title29537184').hide();
+$("#id123-control29537187").focusout(function() {
+  if ($("#id123-control29537187") === '') {
+      $('#id123-title29537187').hide();
   } else {
-      $('#id123-title29537184').show();
+      $('#id123-title29537187').show();
   }
 });
