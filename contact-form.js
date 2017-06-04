d55c55b79798993455f832d994d178771f03dddf
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-04 10:30:03 +0300

    Update contact-form.js

--- contact-form.js
+++ contact-form.js
@@ -2,7 +2,7 @@ $("#id123-control29537184").attr("placeholder", "Nume si Prenume");
 $('#id123-title29537184').hide();
 $("#id123-control29537184").off("focusin");
 $("#id123-control29537184").off("focusout");
-$("#id123-control29537184").click(function() {
+$("#id123-control29537184").on( "click", function() {
   if ($("#id123-control29537184") === '') {
       $('#id123-title29537184').show();
   }
