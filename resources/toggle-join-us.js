ab8df0f637acb11c518fdec5e2ef0c6535b68c4e
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-18 18:50:24 +0300

    Update toggle-join-us.js

--- resources/toggle-join-us.js
+++ resources/toggle-join-us.js
@@ -8,13 +8,16 @@
 	$('#tab-'+ current_hash).trigger('click');
 	
 	function toggleJoinUs() {
+		
 		var selected_form_id = $(this).attr('id') + '-form';
 		var selected_arrow_id = selected_form_id + '-arrow';
 		var current_form_id = $('.join-us-tab-forms:visible').attr('id');
 		var current_arrow_id = current_form_id + '-arrow';
-		if (selected_form_id == current_form_id) {
+		if ($(this).hasClass('join-us-tab-selected')) {
 		   return;
 		} else {
+		   $('.join-us-tab-selected').removeClass('join-us-tab-selected');
+		   $(this).addClass('join-us-tab-selected');
                    $('#' + current_form_id).hide();
 		   $('#' + current_arrow_id).hide();
 		   $('#' + selected_form_id).show();
