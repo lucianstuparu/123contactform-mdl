c388d1c49339003d1e555ca64aabccedb7090d0c
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-18 23:42:36 +0300

    Update contact-form.js

--- contact-form.js
+++ contact-form.js
@@ -1,4 +1,4 @@
-$("div.subitem input").on("focusout", function() {
+$("div.subitem input, document").on("focusout", function() {
  if($(this).val() === "") {
     $("#" + $(this).attr('id').replace('control', 'title')).removeClass('active');
  } else {
