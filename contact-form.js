a7e99be72942d95729e143030c4923d00cd92ab5
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-19 09:50:19 +0300

    Update contact-form.js

--- contact-form.js
+++ contact-form.js
@@ -8,6 +8,11 @@ $("div.subitem input, div.subitem textarea").each(function() {
  }
 });
 
+$("div.subitem input[type="checkbox"]").each(function() {
+ var input_id = $(this).attr('id');
+ $('label[for="'+input_id+'"].class123-fieldname').addClass('active');
+});
+
 $('div.subitem span.fileinput-button').each(function() {
  var input_id = $(this).siblings('input').eq(0).attr('id');
  $("#" + input_id.replace('control', 'title')).addClass('active');
