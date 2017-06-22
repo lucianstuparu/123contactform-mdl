b9339089c343bee060af9437fb083a66ad8f6d94
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-22 05:23:58 +0300

    Update project-cards.js

--- resources/project-cards.js
+++ resources/project-cards.js
@@ -1,1 +1,6 @@
-
+(function($){
+	$('.home-project-blurb').on('click', function() {
+    window.location = $(this).find(".et_pb_main_blurb_image a").attr("href"); 
+    return false;
+  });
+})(jQuery);
