b1c4b7a5756e86f324fd30c38c34c25042dfc4ed
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-26 01:08:38 +0300

    Create map-embed.js

---
+++ resources/map-embed.js
@@ -0,0 +1,7 @@
+(function($){
+	$('.map-container')
+	.click(function(){
+			$(this).find('iframe').addClass('clicked')})
+	.mouseleave(function(){
+			$(this).find('iframe').removeClass('clicked')});
+})(jQuery);
