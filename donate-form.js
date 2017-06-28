2c39ed5b7c4c3b19c8f290579f9f7bfd9ab12e0b
Lucian Stuparu <lucianstuparu@gmail.com>
2017-06-28 20:58:48 +0300

    Update donate-form.js

--- donate-form.js
+++ donate-form.js
@@ -1,7 +1,23 @@
 $('#id123-control30234080_0').on('click', function() {
-		if ($(this).is(':checked')) {
-		    $('#formwrapper').css('padding-top', '25px');
-		} else {
-			
-		}
-	});
+	if ($(this).is(':checked')) {
+		$('#formwrapper').each(function () {
+    			this.style.setProperty( 'padding-top', '25px', 'important' );
+		});
+		$('#id123-control30234080_0 .label-text').each(function () {
+    			this.style.setProperty( 'color', '#fff', 'important' );
+		});
+		$('input[type="checkbox"]:not(old)+label>.outside').each(function () {
+    			this.style.setProperty( 'border-color', '#fff', 'important' );
+		});
+	} else {
+		$('#formwrapper').each(function () {
+    			this.style.setProperty( 'padding-top', '45px', 'important' );
+		});
+		$('#id123-control30234080_0 .label-text').each(function () {
+    			this.style.setProperty( 'color', '#99ffff ', 'important' );
+		});
+		$('input[type="checkbox"]:not(old)+label>.outside').each(function () {
+    			this.style.setProperty( 'border-color', '#99ffff ', 'important' );
+		});
+	}
+});
