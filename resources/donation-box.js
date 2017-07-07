d297089ad8de94cfa79d7e8ef4d032500950132e
Lucian Stuparu <lucianstuparu@gmail.com>
2017-07-07 12:47:57 +0300

    Update donation-box.js

--- resources/donation-box.js
+++ resources/donation-box.js
@@ -1,1 +1,8 @@
+window.addEventListener("message", function(event) {
+  if (event.origin != 'http://www.123contactform.com') {
+    console.log('wrong domain:' + event.origin);
+    return;
+  }
 
+  console.log( "received: " + event.data );
+});
