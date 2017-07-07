5e09985cc819d726c86188324628a2cc7b9cd7c2
Lucian Stuparu <lucianstuparu@gmail.com>
2017-07-07 13:47:16 +0300

    Update donation-box.js

--- resources/donation-box.js
+++ resources/donation-box.js
@@ -1,6 +1,6 @@
 (function($){
 	window.addEventListener("message", function(event) {
-    if (event.origin != 'https://www.123contactform.com') {
+    if (event.origin != 'https://www.123contactform.com' OR event.data.charAt(0) != '|') {
       return;
     }
 
@@ -9,9 +9,11 @@
     $('#donate-box-header2 span.header-amount').html(data[0]);
     $('#donate-box-header1').html($('#donate-box-header2').html());
     
-    $('#donate-box-step2 input.paypal-amount').val(data[0]);
-    $('#donate-box-step2 input.paypal-fullname').val(data[1]);
-    $('#donate-box-step2 input.paypal-email').val(data[2]);
+    $('#donate-box-step2 input.paypal-amount').val(data[1]);
+    $('#donate-box-step2 input.paypal-fullname').val(data[2]);
+    $('#donate-box-step2 input.paypal-email').val(data[3]);
+    $('#donate-box-step2 input.paypal-city').val(data[4]);
+    $('#donate-box-step2 input.paypal-custom').val(data[5]);
     $('#donate-box-step1').html($('#donate-box-step2').html());
     
     console.log( "received: " + event.data );
