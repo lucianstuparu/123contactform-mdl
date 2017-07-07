fb816e89c3fe064eabcde170f484b60c51ea9359
Lucian Stuparu <lucianstuparu@gmail.com>
2017-07-07 13:18:58 +0300

    Update donation-box.js

--- resources/donation-box.js
+++ resources/donation-box.js
@@ -1,8 +1,19 @@
-window.addEventListener("message", function(event) {
-  if (event.origin != 'http://www.123contactform.com') {
-    console.log('wrong domain:' + event.origin);
-    return;
-  }
+(function($){
+	window.addEventListener("message", function(event) {
+    if (event.origin != 'http://www.123contactform.com') {
+      return;
+    }
 
-  console.log( "received: " + event.data );
-});
+    var data = event.data.split('|');
+    
+    $('#donate-box-header2 span.header-amount').html(data[0]);
+    $('#donate-box-header1').html($('#donate-box-header2').html());
+    
+    $('#donate-box-step2 input.paypal-amount').val(data[0]);
+    $('#donate-box-step2 input.paypal-fullname').val(data[1]);
+    $('#donate-box-step2 input.paypal-email').val(data[2]);
+    $('#donate-box-step1').html($('#donate-box-step2').html());
+    
+    console.log( "received: " + event.data );
+  });
+})(jQuery);
