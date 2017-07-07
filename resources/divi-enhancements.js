21ef6179160684af163daaab815a6992385fc0e3
Lucian Stuparu <lucianstuparu@gmail.com>
2017-07-07 07:01:58 +0300

    Update divi-enhancements.js

--- resources/divi-enhancements.js
+++ resources/divi-enhancements.js
@@ -9,34 +9,4 @@
     }
     return false;
   });
-	$(document).ready( function(){
-		function et_change_primary_nav_position( delay ) {
-			setTimeout( function() {
-				var $body = $('body'),
-					$wpadminbar = $( '#wpadminbar' ),
-					$top_header = $( '#top-header' ),
-					et_primary_header_top = 0;
-
-				if ( $wpadminbar.length ) {
-					et_primary_header_top += $wpadminbar.innerHeight();
-				}
-
-				if ( $top_header.length && $top_header.is(':visible') ) {
-					et_primary_header_top += $top_header.innerHeight();
-				}
-				
-				/** changed by lucian3 
-					initially
-					if ( ! window.et_is_vertical_nav && ( $body.hasClass( 'et_fixed_nav' ) )) {
-				*/
-				
-				console.log('lucian3');
-
-				if ( ! window.et_is_vertical_nav && ( $body.hasClass( 'et_fixed_nav' ) ) && et_primary_header_top != 0 ) {
-					
-					$('#main-header').css( 'top', et_primary_header_top );
-				}
-			}, delay );
-		}
-	});
 })(jQuery);
